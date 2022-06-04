616cf9738a14b1aff286e0ca97d1e42a4fc13081
AntoineBlondon <antoine@blondon.fr>
2022-06-04 16:44:10 +0200

    Update main.py

--- echecodames/main.py
+++ echecodames/main.py
@@ -1,7 +1,5 @@
 #Class 7 file
-from bot import plateauxPossibles
 from game_functions import *
-from time import sleep
 
 
 #sleep(2) Pour le bot, on fait croire qu'il réfléchit en le faisant attendre randint(1 to 5) TODO pas oublier de mettre un logo de chargement (pour pas croire que le programme plante)
